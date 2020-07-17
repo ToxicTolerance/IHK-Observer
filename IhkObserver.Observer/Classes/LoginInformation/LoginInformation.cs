fab0b65c605c54cd6589a09baa2273a7a7ab5d01
ToxicTolerance <armin.wild@protonmail.ch>
2020-07-17 13:38:33 +0200

    test

--- IhkObserver.Observer/Classes/LoginInformation/LoginInformation.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IhkObserver.Observer.Classes
-{
-    public struct LoginInformation
-    {
-        public string CaptchaUrl { get; set; }
-        public string ViewState { get; set; }
-        public string ViewStateGenerator { get; set; }
-    }
-}
