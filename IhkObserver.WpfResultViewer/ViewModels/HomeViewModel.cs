42cb4a81cae40d1a50b0643fbc93b0d3c9ec7889
ToxicTolerance <armin.wild@protonmail.ch>
2020-07-17 13:27:23 +0200

    removed id

--- IhkObserver.WpfResultViewer/ViewModels/HomeViewModel.cs
+++ IhkObserver.WpfResultViewer/ViewModels/HomeViewModel.cs
@@ -83,7 +83,7 @@ namespace IhkObserver.WpfResultViewer.ViewModels
 
         public HomeViewModel(MainViewModel mainViewModel) : base(mainViewModel)
         {
-            XmlConfig config = new XmlConfig("2508139", "20735");
+            XmlConfig config = new XmlConfig("00000", "000000");
             config.SaveXmlConfig(Directory.GetCurrentDirectory() + "\\config.xml");
 
             _observer = new Observer.IhkObserver(_welcome, _login, _results, config);
@@ -96,45 +96,6 @@ namespace IhkObserver.WpfResultViewer.ViewModels
         }
 
 
-        private void Login()
-        {
-            //URL's
-            string welcome = "https://ausbildung.ihk.de/pruefungsinfos/Peo/Willkommen.aspx?knr=155";
-            string login = "https://ausbildung.ihk.de/pruefungsinfos/Peo/Login.aspx";
-            string results = "https://ausbildung.ihk.de/pruefungsinfos/Peo/Ergebnisse.aspx";
-
-            // Create a config with your credentials
-            XmlConfig config = new XmlConfig("0000000", "00000");
-            config.SaveXmlConfig(Directory.GetCurrentDirectory() + "\\config.xml");
-
-            //Create new Instance of observer
-            Observer.IhkObserver observer = new Observer.IhkObserver(welcome, login, results, config);
-
-            //Adds a session Id for this session
-            observer.AddSessionId();
-
-            //Extracts loginInformations (URL to ViewState etc. -> ASPX) from the Website
-            observer.GetLoginInformations();
-
-            //Get the captcha as bitmap
-            Bitmap map = _observer.GetLoginCaptcha();
-
-            // Try to solve the captcha
-            //returns a manipulated (applied some filtering etc.) version of input Bitmap            
-            //and outputs the (hopefully correct) string via out-keyword
-            string text;
-            Bitmap mapFiltered = CaptchaSolver.CaptchaSolver.DeCaptcha(map, out text);
-            
-            // Attempt login
-            bool loggedIn = observer.Login(text);
-
-            //If successfully logged in, try to get the Exams Results
-            if (loggedIn == true)
-            {
-                List<SubjectMarks> marks = new List<SubjectMarks>();
-                observer.GetExamInformation(out marks);
-            }
-        }
 
         private void ExecuteLoadResults()
         {
